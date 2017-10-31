9d86599fb8edab266a1d032ce4f0d82f40fc640b
sebpmtl <sebpmtl@gmail.com>
2017-10-31 19:24:41 -0400

    before search csv parsed

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app id="app" dark>
+    <v-navigation-drawer
+      persistent
+      v-model="drawer"
+      enable-resize-watcher
+      right
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark fixed app>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>Mon Appartement</v-toolbar-title>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    </v-toolbar>
+    <main>
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout justify-center align-center>
+                
+            <router-view></router-view>
+          </v-layout>
+        </v-container>
+      </v-content>
+    </main>
+    <v-footer app>
+      <v-spacer></v-spacer>
+      <span class="white--text">&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data: () => ({
+      drawer: true
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
+   
+
+
+
+<style>
+
+</style>
---
+++ src/components/SearchBox.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-card flat>
+      <v-card-text>
+        <v-container>
+          <v-layout row wrap>
+            <v-flex xs12 sm12>
+              <v-select
+                v-bind:items="arrondissements"
+                v-model="a1"
+                label="Recherche"
+                autocomplete
+              ></v-select>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card-text>
+    </v-card>
+</template>
+
+<script>
+import Papa from '../../node_modules/papaparse'
+
+export default {
+  name: 'SearchBox',
+  data () {
+    return {
+       a1: null,
+      arrondissements: []
+      
+      
+    }
+  },
+  
+  mounted(){
+   
+  
+   const url = '../../static/logements-sociaux20170822.csv';
+
+   var parsed = Papa.parse(url,{
+       download: true,
+       header: true,
+       
+       complete: function(results, file) {
+      console.log("Parsing complete:", results.data);
+
+         }
+       
+  })
+ 
+       
+}
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
